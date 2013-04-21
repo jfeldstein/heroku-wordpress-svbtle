c0b7d8e8afc753230965813dbf617553144fd5b5
Jordan Feldstein <jordan@isocket.com>
2013-04-21 13:37:32 -0700

    No related posts (needs more fodder, and a better way of doing it than with kudos)

--- wp-content/themes/svbtle/single.php
+++ wp-content/themes/svbtle/single.php
@@ -73,27 +73,30 @@ get_header(); ?>
 
 <?php get_template_part( 'loop', 'index' ); ?>
 
-<?php
-$posts=$wpdb->get_results($wpdb->prepare(
- "SELECT post_id, meta_value FROM $wpdb->postmeta WHERE meta_key = %s AND post_id <> %d " .
- "ORDER BY CHAR_LENGTH(meta_value) DESC, meta_value DESC LIMIT 5",
- '_wp-svbtle-kudos', get_the_ID()
-));
-?>
+<?php // NO RELATED POSTS ?>
+<?php if(0) : ?>
+  <?php
+  $posts=$wpdb->get_results($wpdb->prepare(
+   "SELECT post_id, meta_value FROM $wpdb->postmeta WHERE meta_key = %s AND post_id <> %d " .
+   "ORDER BY CHAR_LENGTH(meta_value) DESC, meta_value DESC LIMIT 5",
+   '_wp-svbtle-kudos', get_the_ID()
+  ));
+  ?>
 
-<div>
-  <h2 id='also-read-title'>Also read...</h2>
-  <ul id='also-read-items'>
-  <?php for($i=0; $i<count($posts); $i++): ?>
-    <li>
-      <a href="<?php echo get_permalink($posts[$i]->post_id); ?>">
-        <h3><?php echo get_the_title($posts[$i]->post_id); ?></h3>
-        <p class="link_kudo"><?php echo $posts[$i]->meta_value; ?></p>
-      </a>
-    </li>
-  <?php endfor; ?>
-  </ul>
-</div>
+  <div>
+    <h2 id='also-read-title'>Also read...</h2>
+    <ul id='also-read-items'>
+    <?php for($i=0; $i<count($posts); $i++): ?>
+      <li>
+        <a href="<?php echo get_permalink($posts[$i]->post_id); ?>">
+          <h3><?php echo get_the_title($posts[$i]->post_id); ?></h3>
+          <p class="link_kudo"><?php echo $posts[$i]->meta_value; ?></p>
+        </a>
+      </li>
+    <?php endfor; ?>
+    </ul>
+  </div>
+<?php endif; // END OF NO RELATED POSTS ?>
 
 <?php comments_template(); ?>
 
